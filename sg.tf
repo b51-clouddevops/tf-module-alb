d81ef2bd37d3069b96444d340836040d58bf0126
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:58:25 +0530

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -24,3 +24,30 @@ resource "aws_security_group" "allows_mysql" {
     Name = "roboshop-public-alb-${var.ENV}"
   }
 }
+
+# Creates Security Group for Private ALB
+resource "aws_security_group" "allows_mysql" {
+  name               = "roboshop-private-alb-${var.ENV}"
+  description        = "roboshop-private-alb-${var.ENV}"
+  vpc_id             = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "Allow http from frontend only"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-private-alb-${var.ENV}"
+  }
+}
