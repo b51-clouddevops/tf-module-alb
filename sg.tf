85932cc96446dfec56c4486240bd428b0942c309
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:55:15 +0530

    Auto Push Commit

---
+++ sg.tf
@@ -0,0 +1,34 @@
+# Creates Security Group for Public ALB
+resource "aws_security_group" "allows_mysql" {
+  name               = "roboshop-${var.ENV}-mysql"
+  description        = "roboshop-${var.ENV}-mysql"
+  vpc_id             = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "Allow mysql from default VPC"
+    from_port        = var.RDS_MYSQL_PORT
+    to_port          = var.RDS_MYSQL_PORT
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
+    description      = "Allow mysql from default VPC"
+    from_port        = var.RDS_MYSQL_PORT
+    to_port          = var.RDS_MYSQL_PORT
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql"
+  }
+}
