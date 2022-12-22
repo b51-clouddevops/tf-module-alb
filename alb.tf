2d9e1c5c2cd46650d2ef1cb5daa75f09d348d3cf
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 08:20:23 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
+  security_groups    = var.INTERNAL ? aws_security_group.alb_private.*.id : aws_security_group.alb_public.*.id
   subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID
 
 
