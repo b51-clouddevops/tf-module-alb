8ecd666c7c356227155df6829eb23aa7632ba71f
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:45:18 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -2,11 +2,5 @@ module "vpc" {
   source                   = "./vendor/modules/vpc"  
 }
 
-# module "vpc" {
-#   source = "git::https://github.com/b51-clouddevops/tf-module-vpc.git"
-# }
-
-# We cannot parameterise anything on source of the module. So that means we cannot use the variables.
-# How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
-# Alternatives to make it flexible ?
+We can either go with write code once, use the same for public and private loadbalancers or write individual code for public & private alb 
 
