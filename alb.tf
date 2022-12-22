5faf6589b0c0fddede7b174b258fd618fee62eb1
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:50:44 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "alb" {
-  name               = 
+  name               = var.ALB_NAME
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
