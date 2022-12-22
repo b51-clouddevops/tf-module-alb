d32b9135d2214213350795ad790872cf5b572d0a
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 08:19:40 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -5,15 +5,8 @@ resource "aws_lb" "alb" {
   security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
   subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID
 
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
 
   tags = {
-    Environment = "production"
+    Environment = var.ALB_NAME
   }
 }
