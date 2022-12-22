6e344a01260c96729e4a96791f6b638be1d46a82
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:46:56 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# This should only be called for the frontend component
 module "alb-public" {
   source                   = "./vendor/modules/alb"  
 }
 
+# This should only be called for the backend component
 module "alb-private" {
   source                   = "./vendor/modules/alb"  
 }
