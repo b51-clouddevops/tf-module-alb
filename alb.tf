bf9425121ada4ec8f4c48d5f4fc0ef099431ca3b
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 08:05:56 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "alb" {
   internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = 
 
   enable_deletion_protection = true
 
