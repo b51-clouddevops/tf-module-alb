e496271badecb5ac2544d1b40c69e9a64a67fc96
CodingManoj <mvp2612@hotmail.com>
2022-12-25 08:36:07 +0530

    Auto Push Commit

--- listerner.tf
+++ listerner.tf
@@ -1,7 +1,7 @@
 # This should be created for internal or private load balancer only
 resource "aws_lb_listener" "private" {
-  count             = var
-  load_balancer_arn = aws_lb.front_end.arn
+  count             = var.INTERNAL ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
