50b79f092f35fada4cbb1191ccd86e1a27ea0480
CodingManoj <mvp2612@hotmail.com>
2022-12-25 08:37:09 +0530

    Auto Push Commit

--- listerner.tf
+++ listerner.tf
@@ -2,13 +2,17 @@
 resource "aws_lb_listener" "private" {
   count             = var.INTERNAL ? 1 : 0
   load_balancer_arn = aws_lb.alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  port              = "80"
+  protocol          = "HTTP"
+
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
   }
 }
